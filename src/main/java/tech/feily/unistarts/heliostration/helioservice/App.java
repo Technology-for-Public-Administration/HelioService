944d4c0cb76c988f819aff6478a69eddc0191054
Feily Zhang <fei472625@foxmail.com>
2020-04-24 18:02:01 +0800

    update version number

--- src/main/java/tech/feily/unistarts/heliostration/helioservice/App.java
+++ src/main/java/tech/feily/unistarts/heliostration/helioservice/App.java
@@ -21,7 +21,7 @@ public class App {
     
     public static void main( String[] args ) throws Exception {
         System.out.println("Welcome to the HelioChain platform(Service Node).");
-        System.out.println("Current application version : Alpha 0.1.0.0423");
+        System.out.println("Current application version : Alpha 0.0.1.0423");
         System.out.println("This application is licensed through GNU General Public License version 3 (GPLv3).");
         System.out.println("Copyright \u00A92020 tpastd.com. All rights reserved.\n");
         System.out.println("First, you need to add some configuration information to use.");
