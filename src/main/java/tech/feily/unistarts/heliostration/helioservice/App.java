143bfdc7cf960ccaf7e508598d0c911edfcf0c43
FeilyZhang <contact@tpastd.com>
2020-04-25 21:04:21 +0800

    update sth

--- src/main/java/tech/feily/unistarts/heliostration/helioservice/App.java
+++ src/main/java/tech/feily/unistarts/heliostration/helioservice/App.java
@@ -23,7 +23,7 @@ public class App {
         System.out.println("Welcome to the HelioChain platform(Service Node).");
         System.out.println("Current application version : Alpha 0.0.1.0423");
         System.out.println("This application is licensed through GNU General Public License version 3 (GPLv3).");
-        System.out.println("Copyright \u00A92020 tpastd.com. All rights reserved.\n");
+        System.out.println("Copyright (c)2020 tpastd.com. All rights reserved.\n");
         System.out.println("First, you need to add some configuration information to use.");
         System.out.println("------------------------------------------------------------------");
         PreCmd.run();
